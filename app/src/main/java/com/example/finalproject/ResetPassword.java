83a3b5c4a31f1c3fa9c0251f7205df7ab55bb7f1
jatinder174@gmail.com <rubaljpsjp>
2020-06-04 00:42:49 -0400

    GIT CHECK

--- app/src/main/java/com/example/finalproject/ResetPassword.java
+++ app/src/main/java/com/example/finalproject/ResetPassword.java
@@ -3,12 +3,14 @@ package com.example.finalproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class ResetPassword extends AppCompatActivity {
-
+Button hello;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reset_password);
+
     }
 }
