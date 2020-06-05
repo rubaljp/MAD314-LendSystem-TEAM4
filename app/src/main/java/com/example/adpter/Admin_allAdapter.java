d324b891710799ad8a5413876a29af8d026fcdc7
jatinder mann <jatindermann86@gmail.com>
2020-06-04 21:41:21 -0400

    Admin adapters added

---
+++ app/src/main/java/com/example/adpter/Admin_allAdapter.java
@@ -0,0 +1,52 @@
+package com.example.adpter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.finalproject.R;
+
+public class Admin_allAdapter extends RecyclerView.Adapter<Admin_allAdapter.ViewHolder>{
+
+    Context context;
+
+    // RecyclerView recyclerView;
+    public Admin_allAdapter(Context context) {
+        this.context = context;
+
+    }
+    @Override
+    public Admin_allAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        View listItem= layoutInflater.inflate(R.layout.listitems_row, parent, false);
+        ViewHolder viewHolder = new ViewHolder(listItem);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return 10;
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        public ImageView imageView;
+        public TextView textView;
+        public ViewHolder(View itemView) {
+            super(itemView);
+
+
+        }
+    }
+}
+
