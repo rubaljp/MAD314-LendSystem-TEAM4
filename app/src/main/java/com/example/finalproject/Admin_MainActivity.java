e6167a41c68bd0ce873d02b35c9badff67050d56
Arsh <pritsidhu1995@gmail.com>
2020-06-06 19:28:12 -0400

    Admin_MainActivity Updated

--- app/src/main/java/com/example/finalproject/Admin_MainActivity.java
+++ app/src/main/java/com/example/finalproject/Admin_MainActivity.java
@@ -2,15 +2,42 @@ package com.example.finalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.example.finalproject.R;
 
 public class Admin_MainActivity extends AppCompatActivity {
 
+    TextView view_item,Add_item,Logout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin__main);
+
+        view_item = findViewById(R.id.view_item);
+        Add_item = findViewById(R.id.Add_item);
+        Logout = findViewById(R.id.Logout);
+
+        view_item.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent i=new Intent(Admin_MainActivity.this, View_all_list.class);
+                startActivity(i);
+            }
+        });
+        Add_item.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent i=new Intent(Admin_MainActivity.this, Add_items.class);
+                startActivity(i);
+            }
+        });
+
     }
 }
+
