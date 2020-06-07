bd84b2c4fa1fdd3d39ff194bff398c2dd9ac83c4
sahil sachdeva <sahil.sachdeva1193@gmail.com>
2020-06-06 22:16:34 -0400

    Forgot password and various intents added

--- app/src/main/java/com/example/finalproject/Add_items.java
+++ app/src/main/java/com/example/finalproject/Add_items.java
@@ -3,12 +3,22 @@ package com.example.finalproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class Add_items extends AppCompatActivity {
 
+    ImageView back;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_items);
+        back = findViewById(R.id.back);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
--- app/src/main/java/com/example/finalproject/View_all_list.java
+++ app/src/main/java/com/example/finalproject/View_all_list.java
@@ -1,14 +1,54 @@
 package com.example.finalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.example.finalproject.adpter.Admin_allAdapter;
 
 public class View_all_list extends AppCompatActivity {
 
+    RecyclerView viewall_item;
+    Admin_allAdapter admin_allAdapter;
+    ImageView back,add;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_all_list);
+
+        viewall_item =findViewById(R.id.viewall_item);
+
+
+        LinearLayoutManager LayoutManagaer
+                = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+        viewall_item.setLayoutManager(LayoutManagaer);
+
+        admin_allAdapter = new Admin_allAdapter(this);
+        viewall_item.setAdapter(admin_allAdapter);
+
+        back=findViewById(R.id.back);
+        add=findViewById(R.id.add);
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i=new Intent(View_all_list.this, Add_items.class);
+                startActivity(i);
+            }
+        });
+
     }
 }
