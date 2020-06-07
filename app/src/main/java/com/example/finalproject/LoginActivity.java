bca179fea02c92c1583cd7ce72f89b6d00ab4ab2
jatinder174@gmail.com <rubaljpsjp>
2020-06-06 22:27:00 -0400

    Forget password page with previous errors resolved

--- app/src/main/java/com/example/finalproject/LoginActivity.java
+++ app/src/main/java/com/example/finalproject/LoginActivity.java
@@ -4,17 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
-    TextView donotaccount;
+    TextView donotaccount,forgot;
     EditText email,password;
     Button login;
+    RadioGroup group;
 
     RadioButton admin,user;
     @Override
@@ -25,9 +28,11 @@ public class LoginActivity extends AppCompatActivity {
         donotaccount=findViewById(R.id.donotaccount);
         email=findViewById(R.id.usernameLogin);
         password=findViewById(R.id.passwordLogin);
-        login=findViewById(R.id.buttonsubmit);
+        login=findViewById(R.id.buttonLogin);
         admin=findViewById(R.id.admin);
         user=findViewById(R.id.user);
+        forgot=findViewById(R.id.forgot);
+        group=findViewById(R.id.group);
 
 
         donotaccount.setOnClickListener(new View.OnClickListener() {
@@ -46,6 +51,14 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
+        forgot.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent i=new Intent(LoginActivity.this,Forgot_password.class);
+                startActivity(i);            }
+        });
+
     }
 
 
@@ -57,13 +70,18 @@ public class LoginActivity extends AppCompatActivity {
         }else  if (password.getText().toString().length()  == 0){
             GlobalClass.showtost(this," Please enter password");
         }else {
-
-            if (admin.getText().toString().equals("admin")){
+            int selectedId=group.getCheckedRadioButtonId();
+            admin=findViewById(selectedId);
+            Log.d("dfsdfsdf",admin.getText().toString());
+            Log.d("dfsdfsdf",user.getText().toString());
+            if (admin.getText().toString().equals("Admin")){
                 Intent i=new Intent(LoginActivity.this, Admin_MainActivity.class);
                 startActivity(i);
-            }else  if (user.getText().toString().equals("User")){
+                finish();
+            }else  if (admin.getText().toString().equals("User")){
                 Intent i=new Intent(LoginActivity.this, MainActivity.class);
                 startActivity(i);
+                finish();
             }
 
 
