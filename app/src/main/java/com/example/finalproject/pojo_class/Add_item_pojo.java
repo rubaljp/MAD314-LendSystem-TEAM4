17ae975259376e0221ad473809dee5276453fb6a
Arsh <pritsidhu1995@gmail.com>
2020-06-10 22:24:38 -0400

    Add_item_pojo class added

---
+++ app/src/main/java/com/example/finalproject/pojo_class/Add_item_pojo.java
@@ -0,0 +1,42 @@
+package com.example.finalproject.pojo_class;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class Add_item_pojo {
+
+    @SerializedName("error")
+    @Expose
+    private Boolean error;
+    @SerializedName("status")
+    @Expose
+    private Integer status;
+    @SerializedName("message")
+    @Expose
+    private String message;
+
+    public Boolean getError() {
+        return error;
+    }
+
+    public void setError(Boolean error) {
+        this.error = error;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
+
