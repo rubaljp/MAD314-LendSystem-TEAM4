28195154130f035a0960754bd47393a69445961f
jatinder mann <jatindermann86@gmail.com>
2020-06-12 18:17:50 -0400

    ApiClient updated

---
+++ app/src/main/java/com/example/finalproject/interface_api/ApiClient.java
@@ -0,0 +1,48 @@
+package com.example.finalproject.interface_api;
+
+
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.converter.scalars.ScalarsConverterFactory;
+
+
+public class ApiClient {
+
+    private static retrofit2.Retrofit retrofit = null;
+
+    public static ApiInterface getClient() {
+
+        if (retrofit == null) {
+            HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+            interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+            OkHttpClient client = new OkHttpClient.Builder().addInterceptor(interceptor).build();
+            Gson gson = new GsonBuilder()
+                    .setLenient()
+                    .create();
+
+           /* Retrofit retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .client(client)
+                    .addConverterFactory(GsonConverterFactory.create(gson))
+                    .build();*/
+
+            retrofit = new retrofit2.Retrofit.Builder()
+                    .baseUrl("http://demo.nhplearning.com/api/v1/")
+                    .client(client)
+                    .addConverterFactory(GsonConverterFactory.create(gson))
+                    .addConverterFactory(ScalarsConverterFactory.create())
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+
+        }
+
+        return retrofit.create(ApiInterface.class);
+    }
+
+
+}
